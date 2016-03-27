acbfd119150051c01da3d8b0b1f5081ed30b36ff
Otto from Mac <operdeck@gmail.com>
2016-03-27 21:55:19 +0200

    LB 0.832216 xgbGrid <- expand.grid(nrounds = seq(10,500,by=50),#seq(500,1000,by=200),                        eta = c(0.02, 0.05, 0.1, 0.2, 0.3), # 0.01                        max_depth = seq(2,7),                        gamma = 2, #c(2, 3, 4, 5),                        colsample_bytree = 0.85,                        min_child_weight = 5)
    The final values used for the model were nrounds = 460, max_depth = 5, eta =
     0.02, gamma = 2, colsample_bytree = 0.85 and min_child_weight = 5.
    Best CV score: 0.8350569

--- predict.R
+++ predict.R
@@ -4,15 +4,18 @@ library(plyr)
 library(tidyr)
 library(dplyr)
 library(xgboost)
+library(DMwR) # outlier detection
+library(bit64)
 
 # Read data
 
-# data col 198, field contains '9999999999' - take care of other warnings
-train <- fread('data/train.csv',integer64 = "character")
-test <- fread('data/test.csv',colClasses=sapply(select(train, -TARGET), class))
+train <- fread('data/train.csv',na.strings="9999999999")
+test <- fread('data/test.csv',na.strings="9999999999",colClasses=sapply(select(train, -TARGET), class))
 train$TARGET <- factor(make.names(train$TARGET)) # values 1/0 will otherwise confuse R
 originalTypes <- sapply(test, class)
 testIDs <- test$ID
+train[, ID := NULL]
+test[, ID := NULL]
 
 # Data analysis
 symCols <- names(test)[!sapply(test, is.numeric)]
@@ -22,25 +25,18 @@ for (symcol in symCols) {
   test[[symcol]] <- as.integer(factor(test[[symcol]],levels=levels))
 }
 
-# Feature engineering
-
-rowWiseCount <- function(x, const) {
-  return( sum(x == const) )
-}
-train$n0 <- apply(train, 1, FUN=rowWiseCount, 0)
-test$n0 <- apply(test, 1, FUN=rowWiseCount, 0)
-
 cat("Train size before variable selection:",dim(train),fill=T)
 
-nzv <- colnames(train)[nearZeroVar(select(train, -TARGET))]
-cat("Removed near-zero variables:", length(nzv), 
+nzv <- colnames(train)[nearZeroVar(select(train, -TARGET), saveMetrics=T)$zeroVar]
+cat("Removed zero variation variables:", length(nzv), 
     "(of", length(names(train)), "):", nzv, fill=T)
 train <- train[,!(names(train) %in% nzv), with=F]
 test  <- test[,!(names(test) %in% nzv), with=F]
 
-cat("Train size after near-zero variance removal:",dim(train),fill=T)
+cat("Train size after (near-)zero variance removal:",dim(train),fill=T)
 
-linearCombos <- colnames(train)[findLinearCombos(select(train, -TARGET))$remove]
+completeCases <- select(train[complete.cases(train)], -TARGET)
+linearCombos <- colnames(train)[findLinearCombos(completeCases)$remove]
 cat("Removed linear combinations:", length(linearCombos), 
     "(of", length(names(train)), "):", linearCombos, fill=T)
 train <- train[,!(names(train) %in% linearCombos),with=F]
@@ -48,14 +44,50 @@ test  <- test[,!(names(test) %in% linearCombos),with=F]
 
 cat("Train size after linear combo removal:",dim(train),fill=T)
 
-trainCor <- cor(select(train, -TARGET), method='spearman')
-correlatedVars <- colnames(train)[findCorrelation(trainCor, cutoff = 0.95, verbose = F)]
-cat("Removed highly correlated cols:", length(correlatedVars), 
-    "(of", length(names(train)), ")", correlatedVars, fill=T)
-train <- train[,!(names(train) %in% correlatedVars),with=F]
-test  <- test[,!(names(test) %in% correlatedVars),with=F]
+if (F) {
+  trainCor <- cor(select(train, -TARGET), method='spearman')
+  correlatedVars <- colnames(train)[findCorrelation(trainCor, cutoff = 0.95, verbose = F)]
+  cat("Removed highly correlated cols:", length(correlatedVars), 
+      "(of", length(names(train)), ")", correlatedVars, fill=T)
+  train <- train[,!(names(train) %in% correlatedVars),with=F]
+  test  <- test[,!(names(test) %in% correlatedVars),with=F]
 
-cat("Train size after highly correlated var removal:",dim(train),fill=T)
+  cat("Train size after highly correlated var removal:",dim(train),fill=T)
+}
+
+# Keep only top-N most important variables
+# topN <- c(head(varImp$field,10), "TARGET")
+# train <- train[, names(train) %in% topN, with=F]
+# test <- test[, names(test) %in% topN, with=F]
+
+# Feature engineering
+
+rowWiseCount <- function(x, const) {
+  return (sum(x == const))
+}
+train$nZero <- apply(train, 1, FUN=rowWiseCount, 0)
+test$nZero <- apply(test, 1, FUN=rowWiseCount, 0)
+
+rowWiseSign <- function(x) {
+  return (sum(x < 0))
+}
+train$nNeg <- apply(train, 1, FUN=rowWiseSign)
+test$nNeg <- apply(test, 1, FUN=rowWiseSign)
+
+# Count outliers per row
+train$nOutliers <- 0
+test$nOutliers <- 0
+for (col in names(test)) {
+  outlierMin <- mean(train[[col]],na.rm=T) - 3*sd(train[[col]],na.rm=T)
+  outlierMax <- mean(train[[col]],na.rm=T) + 3*sd(train[[col]],na.rm=T)
+  train$nOutliers <- train$nOutliers + as.integer(train[[col]] > outlierMax | train[[col]] < outlierMin)
+  test$nOutliers <- test$nOutliers + as.integer(test[[col]] > outlierMax | test[[col]] < outlierMin)
+  cat("Col", col, "train outliers",
+      sum(train[[col]] > outlierMax | train[[col]] < outlierMin, na.rm=T),
+      "test outliers",
+      sum(test[[col]] > outlierMax | test[[col]] < outlierMin, na.rm=T),
+      fill=T)
+}
 
 # Var importance (on a sample otherwise way too slow)
 daSample <- sample_n(train, size=10000)
@@ -70,10 +102,11 @@ p <- ggplot(head(varImp,40),
   scale_y_continuous(labels = scales::percent)
 print(p)
 
-# Keep only top-N most important variables
-# topN <- c(head(varImp$field,10), "TARGET")
-# train <- train[, names(train) %in% topN, with=F]
-# test <- test[, names(test) %in% topN, with=F]
+# Simple NA replacement
+for (col in names(test)) {
+  test[[col]] <- ifelse(is.na(test[[col]]), -1234567, test[[col]])
+  train[[col]] <- ifelse(is.na(train[[col]]), -1234567, train[[col]])
+}
 
 # Build model
 crossValidation <- trainControl(
@@ -86,7 +119,7 @@ crossValidation <- trainControl(
 
 xgbGrid <- expand.grid(nrounds = seq(10,500,by=50),#seq(500,1000,by=200),
                        eta = c(0.02, 0.05, 0.1, 0.2, 0.3), # 0.01
-                       max_depth = seq(10),
+                       max_depth = seq(2,7),
                        gamma = 2, #c(2, 3, 4, 5), 
                        colsample_bytree = 0.85, 
                        min_child_weight = 5) 
@@ -106,7 +139,11 @@ print(plot(varImp(model)))
 # Report on CV tuning
 print(model)
 print(ggplot(model))
-      
+cat("Best CV score:",max(model$results$ROC),fill=T)
+
+# TODO fix NAs
+# Best CV score: 0.8350569
+
 # Score test set
 preds <- predict(model, test, type="prob")
 submission <- data.frame(ID=testIDs, TARGET=preds[,2])
